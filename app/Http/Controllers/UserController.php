1420542eff5523d74d219c7c7c3fff9cd4db1c1c
copallanoselizabeth-cloud <copallanoselizabeth@gmail.com>
2026-07-17 11:00:00 -0400

    Creacion de migracion para tabla estudiantes

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $usuarios = User::where('role', 'cajero')->get();
+        return view('usuarios.index', compact('usuarios'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'cajero',
+        ]);
+
+        return back()->with('success', 'Cajero creado exitosamente.');
+    }
+}
